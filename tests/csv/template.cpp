e3e39f575e20549593935e2e6a2e431acd85df47
Coranna Howard <me@komiga.com>
2011-03-31 03:05:44 -0400

    Implemented ductCSV and extension template class; added test code.
    ---
    * ductCSV
    	* Implemented classes CSVRow, CSVMap, CSVTemplate (extension)
    	* Added test code

---
+++ tests/csv/template.cpp
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <unicode/ustream.h>
+#include <duct/csv.hpp>
+#include <duct/csvtemplate.hpp>
+
+using namespace duct;
+
+const char* inpath="tpl.csv";
+const UChar32 sepchar=',';
+const unsigned int headercount=0;
+
+const CSVTRecord __tpl_layout[]={
+	{VARTYPE_BOOL, false},
+	{VARTYPE_BOOL, false},
+	{VARTYPE_INTEGER, true},
+	{VARTYPE_FLOAT, true},
+	{VARTYPE_ANY, true}
+};
+
+CSVTemplate tpl(5, __tpl_layout);
+CSVTemplate tpl2(5, NULL); // only requires a count of 5
+
+void printRow(const CSVRow& row) {
+	printf("{%s, %s} [%d:%lu] ", tpl.validate(row) ? "true " : "false", tpl2.validate(row) ? "true " : "false", row.getIndex(), row.getCount(true));
+	UnicodeString temp;
+	CSVFormatter::formatRow(row, temp, sepchar);
+	std::cout<<temp<<std::endl;
+}
+
+int main() {
+	CSVMap* map=NULL;
+	try {
+		map=CSVFormatter::loadFromFile(inpath, sepchar, headercount);
+	} catch (CSVParserException e) {
+		printf("caught exception:\n%s\n", e.what());
+		return 1;
+	}
+	if (map) {
+		CSVRowMap::const_iterator iter;
+		for (iter=map->begin(); iter!=map->end(); ++iter) {
+			printRow(*iter->second);
+		}
+	} else {
+		printf("Failed to read %s\n", inpath);
+		return 1;
+	}
+	return 0;
+}
+
