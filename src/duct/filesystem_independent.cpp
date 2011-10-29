c6be29e0a8b57c3da34636d2e700606d1e42b3f6
Coranna Howard <me@komiga.com>
2011-10-28 23:40:24 -0400

    Removed header and inline files from 'files' in premake script.
    Added FileSystem functions (and test code):
    
    * normalizePath()
    * pathHasTrailingSlash()
    * pathHasFilename()
    * pathHasExtension()
    * pathHasLeftPart()
    * pathHasRightPart()
    * pathHasDirectory()
    * extractFileExtension()
    * extractFileDirectory()
    * extractFilename()
    * extractFileLeftPart()
    * extractFileRightPart()

---
+++ src/duct/filesystem_independent.cpp
@@ -0,0 +1,377 @@
+/**
+@file filesystem_independent.cpp
+@author Tim Howard
+
+@section LICENSE
+Copyright (c) 2010-2011 Tim Howard
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include <algorithm>
+#include <duct/debug.hpp>
+#include <duct/filesystem.hpp>
+
+namespace duct {
+
+// FileSystem implementation
+
+namespace FileSystem {
+
+static const UnicodeString g_ustr_dos_slash('\\');
+static const UnicodeString g_ustr_nix_slash('/');
+
+/*bool hasLeadingPeriod(const std::string& path) {
+	size_t sla_pos=path.find_last_of('/');
+	if (std::string::npos==sla_pos) {
+		return 0==path.find_first_of('.');
+	} else {
+		return sla_pos+1==path.find_first_of('.', sla_pos);
+	}
+}
+
+bool hasLeadingPeriod(const UnicodeString& path) {
+	int32_t sla_pos=path.lastIndexOf('/');
+	if (-1==sla_pos) {
+		return 0==path.indexOf('.');
+	} else {
+		return sla_pos+1==path.indexOf('.', sla_pos);
+	}
+}*/
+
+size_t getRightPartPos_(const std::string& path, bool allow_leading, size_t& beg_pos) {
+	size_t sla_pos=path.find_last_of('/');
+	beg_pos=(std::string::npos!=sla_pos) ? sla_pos : 0;
+	size_t dot_pos=path.find_first_of('.', beg_pos);
+	if (allow_leading && (0==dot_pos || beg_pos+1==dot_pos)) {
+		sla_pos=dot_pos+1<path.size() ? path.find_first_of('.', dot_pos+1) : std::string::npos;
+		if (std::string::npos!=sla_pos) {
+			return sla_pos;
+		}
+	}
+	return dot_pos;
+}
+
+size_t getRightPartPos_(const std::string& path, bool allow_leading) {
+	size_t beg_pos;
+	return getRightPartPos_(path, allow_leading, beg_pos);
+}
+
+int32_t getRightPartPos_(const UnicodeString& path, bool allow_leading, int32_t& beg_pos) {
+	int32_t sla_pos=path.lastIndexOf('/');
+	beg_pos=(-1!=sla_pos) ? sla_pos : 0;
+	int32_t dot_pos=path.indexOf('.', beg_pos);
+	if (allow_leading && (0==dot_pos || beg_pos+1==dot_pos)) {
+		sla_pos=dot_pos+1<path.length() ? path.indexOf('.', dot_pos+1) : -1;
+		if (-1!=sla_pos) {
+			return sla_pos;
+		}
+	}
+	return dot_pos;
+}
+
+int32_t getRightPartPos_(const UnicodeString& path, bool allow_leading) {
+	int32_t beg_pos;
+	return getRightPartPos_(path, allow_leading, beg_pos);
+}
+
+size_t getExtensionPos_(const std::string& path) {
+	size_t dot_pos=path.find_last_of('.');
+	if (std::string::npos!=dot_pos) {
+		size_t sla_pos=path.find_last_of('/');
+		if (std::string::npos==sla_pos || dot_pos>sla_pos) {
+			return dot_pos;
+		}
+	}
+	return std::string::npos;
+}
+
+int32_t getExtensionPos_(const UnicodeString& path) {
+	int32_t dot_pos=path.lastIndexOf('.');
+	if (-1!=dot_pos) {
+		int32_t sla_pos=path.lastIndexOf('/');
+		if (-1==sla_pos || dot_pos>sla_pos) {
+			return dot_pos;
+		}
+	}
+	return -1;
+}
+
+void normalizePath(const std::string& path, std::string& result) {
+	if (!path.empty()) {
+		result.reserve(path.size()+1);
+		result.assign(path);
+		std::replace(result.begin(), result.end(), '\\', '/');
+	} else {
+		result.clear();
+	}
+}
+
+void normalizePath(const UnicodeString& path, UnicodeString& result) {
+	if (!path.isEmpty()) {
+		result.setTo(path);
+		result.findAndReplace(g_ustr_dos_slash, g_ustr_nix_slash);
+	} else {
+		result.remove();
+	}
+}
+
+void normalizePath(const std::string& path, std::string& result, bool trailing_slash) {
+	normalizePath(path, result);
+	// don't remove the root path slash, if there is one
+	if (1<result.size()) {
+		std::string::reverse_iterator it=result.rbegin();
+		if (trailing_slash && '/'!=(*it)) {
+			result.push_back('/');
+		} else if (!trailing_slash && '/'==(*it)) {
+			result.erase(--(it.base()));
+		}
+	}
+}
+
+void normalizePath(const UnicodeString& path, UnicodeString& result, bool trailing_slash) {
+	normalizePath(path, result);
+	// don't remove the root path slash, if there is one
+	if (1<result.length()) {
+		UChar c=result[result.length()-1];
+		if (trailing_slash && '/'!=c) {
+			result.append('/');
+		} else if (!trailing_slash && '/'==c) {
+			result.remove(path.length()-1);
+		}
+	}
+}
+
+bool pathHasTrailingSlash(const std::string& path) {
+	std::string::const_reverse_iterator it=path.rbegin();
+	if (path.rend()!=it && '/'==(*it)) {
+		return true;
+	}
+	return false;
+}
+
+bool pathHasTrailingSlash(const UnicodeString& path) {
+	if (!path.isEmpty() && '/'==path[path.length()-1]) {
+		return true;
+	}
+	return false;
+}
+
+bool pathHasFilename(const std::string& path) {
+	if (!path.empty()) {
+		size_t sla_pos=path.find_last_of('/');
+		if (path.size()-1!=sla_pos) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool pathHasFilename(const UnicodeString& path) {
+	if (!path.isEmpty()) {
+		int32_t sla_pos=path.lastIndexOf('/');
+		if (path.length()-1!=sla_pos) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool pathHasExtension(const std::string& path) {
+	if (!path.empty()) {
+		size_t dot_pos=getExtensionPos_(path);
+		return std::string::npos!=dot_pos;
+	}
+	return false;
+}
+
+bool pathHasExtension(const UnicodeString& path) {
+	if (!path.isEmpty()) {
+		int32_t dot_pos=getExtensionPos_(path);
+		return -1!=dot_pos;
+	}
+	return false;
+}
+
+bool pathHasLeftPart(const std::string& path, bool allow_leading) {
+	if (!path.empty()) {
+		size_t beg_pos;
+		size_t dot_pos=getRightPartPos_(path, allow_leading, beg_pos);
+		return std::string::npos!=dot_pos && beg_pos!=dot_pos && beg_pos+1!=dot_pos;
+	}
+	return false;
+}
+
+bool pathHasLeftPart(const UnicodeString& path, bool allow_leading) {
+	if (!path.isEmpty()) {
+		int32_t beg_pos;
+		int32_t dot_pos=getRightPartPos_(path, allow_leading, beg_pos);
+		return -1!=dot_pos && beg_pos!=dot_pos && beg_pos+1!=dot_pos;
+	}
+	return false;
+}
+
+bool pathHasRightPart(const std::string& path, bool allow_leading) {
+	if (!path.empty()) {
+		return std::string::npos!=getRightPartPos_(path, allow_leading);
+	}
+	return false;
+}
+
+bool pathHasRightPart(const UnicodeString& path, bool allow_leading) {
+	if (!path.isEmpty()) {
+		return -1!=getRightPartPos_(path, allow_leading);
+	}
+	return false;
+}
+
+bool pathHasDirectory(const std::string& path) {
+	if (!path.empty()) {
+		size_t sla_pos=path.find_last_of('/');
+		if (std::string::npos!=sla_pos && path.size()-1!=sla_pos) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool pathHasDirectory(const UnicodeString& path) {
+	if (!path.isEmpty()) {
+		int32_t sla_pos=path.lastIndexOf('/');
+		if (-1!=sla_pos && path.length()-1!=sla_pos) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool extractFileExtension(const std::string& path, std::string& result, bool include_period) {
+	if (pathHasExtension(path)) {
+		size_t dot_pos=path.find_last_of('.');
+		result.assign(path, include_period ? dot_pos : dot_pos+1, std::string::npos);
+		return true;
+	}
+	return false;
+}
+
+bool extractFileExtension(const UnicodeString& path, UnicodeString& result, bool include_period) {
+	if (pathHasExtension(path)) {
+		int32_t dot_pos=path.lastIndexOf('.');
+		result.setTo(path, include_period ? dot_pos : dot_pos+1);
+		return true;
+	}
+	return false;
+}
+
+bool extractFileDirectory(const std::string& path, std::string& result, bool trailing_slash) {
+	if (pathHasDirectory(path)) {
+		size_t sla_pos=path.find_last_of('/');
+		result.assign(path, 0, sla_pos);
+		if (trailing_slash || 0==result.size()) {
+			result.push_back('/');
+		}
+		return true;
+	}
+	return false;
+}
+
+bool extractFileDirectory(const UnicodeString& path, UnicodeString& result, bool trailing_slash) {
+	if (pathHasDirectory(path)) {
+		int32_t sla_pos=path.lastIndexOf('/');
+		result.setTo(path, 0, sla_pos);
+		if (trailing_slash || 0==result.length()) {
+			result.append('/');
+		}
+		return true;
+	}
+	return false;
+}
+
+bool extractFilename(const std::string& path, std::string& result, bool with_extension) {
+	if (pathHasFilename(path)) {
+		size_t sla_pos=path.find_last_of('/');
+		size_t dot_pos=with_extension ? std::string::npos : getExtensionPos_(path);
+		sla_pos=(std::string::npos==sla_pos) ? 0 : sla_pos+1;
+		result.assign(path, sla_pos, dot_pos<sla_pos ? std::string::npos : dot_pos-sla_pos);
+		return true;
+	}
+	return false;
+}
+
+bool extractFilename(const UnicodeString& path, UnicodeString& result, bool with_extension) {
+	if (pathHasFilename(path)) {
+		int32_t sla_pos=path.lastIndexOf('/');
+		int32_t dot_pos=with_extension ? INT32_MAX : getExtensionPos_(path);
+		sla_pos=(-1==sla_pos) ? 0 : sla_pos+1;
+		result.setTo(path, sla_pos, dot_pos<sla_pos ? INT32_MAX : dot_pos-sla_pos);
+		return true;
+	}
+	return false;
+}
+
+bool extractFileLeftPart(const std::string& path, std::string& result, bool allow_leading) {
+	if (!path.empty()) {
+		size_t beg_pos;
+		size_t dot_pos=getRightPartPos_(path, allow_leading, beg_pos);
+		if (std::string::npos!=dot_pos && beg_pos!=dot_pos && beg_pos+1!=dot_pos) {
+			result.assign(path, beg_pos, dot_pos-beg_pos);
+			return true;
+		}
+	}
+	return false;
+}
+
+bool extractFileLeftPart(const UnicodeString& path, UnicodeString& result, bool allow_leading) {
+	if (!path.isEmpty()) {
+		int32_t beg_pos;
+		int32_t dot_pos=getRightPartPos_(path, allow_leading, beg_pos);
+		if (-1!=dot_pos && beg_pos!=dot_pos && beg_pos+1!=dot_pos) {
+			result.setTo(path, beg_pos, dot_pos-beg_pos);
+			return true;
+		}
+	}
+	return false;
+}
+
+bool extractFileRightPart(const std::string& path, std::string& result, bool include_period, bool allow_leading) {
+	if (!path.empty()) {
+		size_t dot_pos=getRightPartPos_(path, allow_leading);
+		if (std::string::npos!=dot_pos) {
+			result.assign(path, include_period ? dot_pos : dot_pos+1, std::string::npos);
+			return true;
+		}
+	}
+	return false;
+}
+
+bool extractFileRightPart(const UnicodeString& path, UnicodeString& result, bool include_period, bool allow_leading) {
+	if (!path.isEmpty()) {
+		int32_t dot_pos=getRightPartPos_(path, allow_leading);
+		if (-1!=dot_pos) {
+			result.setTo(path, include_period ? dot_pos : dot_pos+1, INT32_MAX);
+			return true;
+		}
+	}
+	return false;
+}
+
+} // namespace FileSystem
+
+} // namespace duct
+
