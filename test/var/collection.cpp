b169989c030f2f1954d6b028d4b89bfd1972d8ae
Coranna Howard <me@komiga.com>
2013-02-18 23:55:57 -0500

    test/var/collection: use std::initializer_list ctor.

--- test/var/collection.cpp
+++ test/var/collection.cpp
@@ -10,20 +10,21 @@
 #include "common.inl"
 
 int main(int, char*[]) {
-	duct::Variable top(duct::u8string("top"), duct::VARTYPE_ARRAY);
-	top
-		.emplace_back(duct::u8string("string_var"), duct::u8string("bar"))
-		.emplace_back(duct::u8string("int_var"), 12341234)
-		.emplace_back(duct::u8string("float_var"), 3.14159f)
-		.emplace_back(duct::u8string("bool_var"), true)
-		.emplace_back(duct::u8string("null_var"), duct::VARTYPE_NULL)
-		.emplace_back(std::move(
-			duct::Variable(duct::u8string("coll_l1"), duct::VARTYPE_IDENTIFIER)
-			.emplace_back(81354)
-			.emplace_back(true)
-			.emplace_back(duct::VARTYPE_NULL)
-			.emplace_back(duct::VARTYPE_NODE))
-		);
+	duct::Variable top{
+	{"top"}, duct::VARTYPE_ARRAY, {
+		{{"string_var"}, {"bar"}},
+		{{"int_var"}, 12341234},
+		{{"float_var"}, 3.14159f},
+		{{"bool_var"}, true},
+		{{"null_var"}, duct::VARTYPE_NULL},
+		{{"coll_l1"}, duct::VARTYPE_IDENTIFIER, {
+			duct::Variable{81354},
+			duct::Variable{42.0f},
+			duct::Variable{true},
+			duct::Variable{duct::VARTYPE_NULL},
+			duct::Variable{duct::VARTYPE_NODE}
+		}}
+	}};
 	print_var(top);
 	std::cout.flush();
 	return 0;
