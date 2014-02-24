de81d644e78025e5034a92c2d2b54b23bda7da50
Coranna Howard <me@komiga.com>
2014-02-23 21:27:31 -0500

    VariableUtils: added numeric parsing to convert_typed().

--- duct/VariableUtils.hpp
+++ duct/VariableUtils.hpp
@@ -14,6 +14,8 @@ see @ref index or the accompanying LICENSE file for full text.
 #include "./string.hpp"
 #include "./detail/string_traits.hpp"
 #include "./detail/vartype.hpp"
+#include "./StateStore.hpp"
+#include "./CharacterRange.hpp"
 #include "./Variable.hpp"
 
 namespace duct {
@@ -25,97 +27,185 @@ namespace VariableUtils {
 */
 
 namespace {
-static detail::string_traits<detail::var_config::string_type>::char_type const
+using var_char_type
+= detail::string_traits<
+	detail::var_config::string_type
+>::char_type;
+
+static var_char_type const
 	s_sv_false[]{"false"},
 	s_sv_true[]{"true"},
 	s_sv_null[]{"null"}
 ;
+
+static CharacterRange const
+	s_range_numeral{char32{'0'}, '9' - '0'}
+;
+
+bool
+parse_lit(
+	detail::var_config::string_type const& value,
+	var_char_type const* lit
+) {
+	auto it = value.cbegin() + 1;
+	for (
+		++lit;
+		'\0' != *lit && value.cend() != it;
+		++lit, ++it
+	) {
+		if (*it != *lit) {
+			return false;
+		}
+	}
+	if ('\0' == *lit && value.cend() == it) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 } // anonymous namespace
 
 /**
-	Convert string to appropriate type and set Variable value.
+	Convert string to value-class variable.
 
 	@note Conversion is (case sensitive):
-	- @c "false" morphs to a @c VarType::boolean with @c false
-	- @c "true" morphs to a @c VarType::boolean with @c true
-	- @c "null" morphs to a @c VarType::null
-	- else morphs to a @c VarType::string with @c value
+	- @c "false" morphs to a @c VarType::boolean with @c false.
+	- @c "true" morphs to a @c VarType::boolean with @c true.
+	- @c "null" morphs to a @c VarType::null.
+	- @c [+\-]?[0-9.]* morphs to a @c VarType::integer.
+	  or @c VarType::floatp.
+	- Else morphs to a @c VarType::string with @c value.
 
 	@param var Variable to modify.
-	@param value String value to convert.
+	@param value String to convert.
 */
 void
 convert_typed(
 	Variable& var,
-	detail::var_config::string_type value
+	detail::var_config::string_type const& value
 ) {
-	if (0 == value.compare(s_sv_false)) {
-		var.morph(false);
-	} else if (0 == value.compare(s_sv_true)) {
-		var.morph(true);
-	} else if (0 == value.compare(s_sv_null)) {
-		var.nullify();
-	} else {
-		var.morph(std::move(value));
+	bool has_sign = false;
+	bool has_decimal = false;
+	var_char_type const* literal = nullptr;
+
+	if (value.empty()) {
+		goto l_string;
 	}
+
+	// Deduce states from first character
+	switch (value[0]) {
+	case 'f': literal = s_sv_false; break;
+	case 't': literal = s_sv_true; break;
+	case 'n': literal = s_sv_null; break;
+
+	case '-': // fall-through
+	case '+': has_sign = true; break;
+	case '.': has_decimal = true; break;
+	}
+
+	// Try to parse literals
+	if (literal) {
+		if (parse_lit(value, literal)) {
+			switch (value[0]) {
+			case 'f': var.morph(false); break;
+			case 't': var.morph(true); break;
+			case 'n': var.nullify(); break;
+			}
+			return;
+		} else {
+			goto l_string;
+		}
+	}
+
+	// Try to parse numeric value
+	for (
+		auto it = value.cbegin() + unsigned{has_sign || has_decimal};
+		value.cend() != it;
+		++it
+	) {
+		switch (*it) {
+		case '-': // fall-through
+		case '+':
+			// Sign can only occur at start of value
+			goto l_string;
+			break;
+
+		case '.':
+			if (has_decimal) {
+				// Only one decimal can occur
+				goto l_string;
+			} else {
+				has_decimal = true;
+			}
+			break;
+
+		default:
+			if (!s_range_numeral.contains(*it)) {
+				goto l_string;
+			}
+			break;
+		}
+	}
+
+	{
+		aux::istringstream stream(value);
+		if (has_decimal) {
+			var.morph(0.0f);
+			stream >> var.get_float_ref();
+			if (stream.fail()) {
+				var.assign(0.0f);
+			}
+		} else {
+			var.morph(0);
+			stream >> var.get_int_ref();
+			if (stream.fail()) {
+				var.assign(0);
+			}
+		}
+	}
+	return;
+
+l_string:
+	var.reset();
+	var.morph(value);
+	return;
 }
 
 /**
-	Convert string to appropriate Variable.
+	Convert string to value-class variable.
 
-	@note Conversion is (case sensitive):
-	- @c "false" returns a @c VarType::boolean with @c false
-	- @c "true" returns a @c VarType::boolean with @c true
-	- @c "null" returns a @c VarType::null
-	- else returns a @c VarType::string with @c value
+	@returns Variable containing converted value.
+	@param value String to convert.
 
-	@returns String converted to either a @c VarType::boolean,
-	@c VarType::null or @c VarType::string Variable.
-	@param value String value to convert.
+	@sa void convert_typed(Variable&, detail::var_config::string_type const&)
 */
 Variable
 convert_typed(
-	detail::var_config::string_type value
+	detail::var_config::string_type const& value
 ) {
-	if (0 == value.compare(s_sv_false)) {
-		return Variable(false);
-	} else if (0 == value.compare(s_sv_true)) {
-		return Variable(true);
-	} else if (0 == value.compare(s_sv_null)) {
-		return Variable(VarType::null);
-	} else {
-		return Variable(std::move(value));
-	}
+	Variable var;
+	convert_typed(var, value);
+	return var;
 }
 
 /**
-	Convert string to appropriate Variable with name.
+	Convert string to value-class variable with name.
 
-	@note Conversion is (case sensitive):
-	- @c "false" returns a @c VarType::boolean with @c false
-	- @c "true" returns a @c VarType::boolean with @c true
-	- @c "null" returns a @c VarType::null
-	- else returns a @c VarType::string with @c value
+	@returns Variable containing converted value.
+	@param name Name of result variable.
+	@param value String to convert.
 
-	@returns String converted to either a @c VarType::boolean,
-	@c VarType::null or @c VarType::string Variable.
-	@param name Name of constructed variable.
-	@param value String value to convert.
+	@sa void convert_typed(Variable&, detail::var_config::string_type const&)
 */
 Variable
 convert_typed(
 	detail::var_config::name_type name,
-	detail::var_config::string_type value
+	detail::var_config::string_type const& value
 ) {
-	if (0 == value.compare(s_sv_false)) {
-		return Variable(std::move(name), false);
-	} else if (0 == value.compare(s_sv_true)) {
-		return Variable(std::move(name), true);
-	} else if (0 == value.compare(s_sv_null)) {
-		return Variable(std::move(name), VarType::null);
-	} else {
-		return Variable(std::move(name), std::move(value));
-	}
+	Variable var{std::move(name), VarType::null};
+	convert_typed(var, value);
+	return var;
 }
 
 /** @} */ // end of doc-group variable
